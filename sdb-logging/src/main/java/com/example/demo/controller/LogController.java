95f10a3086c19dd2570012e2e65a2ad0b816a2ce
Владислав Юстус <V.Yustus@m-developer.ru>
2025-02-21 17:16:05 +0300

    stable logging in file

--- sdb-logging/src/main/java/com/example/demo/controller/LogController.java
+++ sdb-logging/src/main/java/com/example/demo/controller/LogController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Log;
 import com.example.demo.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LogController {
 
-  LogService logger = new LogService();
+  @Autowired
+  LogService logger;
 
   @PostMapping("/log")
   public void makeLog(
-      @RequestBody Log logEvent
-  ) {
+      @RequestBody Log logEvent) {
     logger.log(logEvent);
   }
 }
--- sdb-logging/src/main/java/com/example/demo/service/LogService.java
+++ sdb-logging/src/main/java/com/example/demo/service/LogService.java
@@ -2,18 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.model.Log;
 
-public class LogService {
+public interface LogService {
 
-  public void log(Log log) {
-    System.out.println(createLogMessage(log));
-  }
-
-  private static String createLogMessage(Log log) {
-    return String.format("%s %s %s - %s",
-        log.timestamp(),
-        log.logLevel(),
-        log.path(),
-        log.message());
-  }
-
+  void log(Log log);
 }
